6f5fa1c36bd267a182088e386f53f7bc2e4b16f8
rulssss <marianopicco83@gmail.com>
2026-08-05 20:05:38 -0300

    comments added

--- python/Service_Status_and_Incident_Monitor/services_monitor.py
+++ python/Service_Status_and_Incident_Monitor/services_monitor.py
@@ -1,5 +1,28 @@
 import json
 
+# 🎯 2. Your Assignment in Python
+
+#Write a Python program (`monitor_servicios.py`) that performs the 
+# following tasks:
+
+# JSON Reading
+#Use Python's native `json` module (`import json`) to open and load
+#  the `servicios.json` file (you can research the `json.load(file)` function).
+
+# Processing with Dictionaries
+# Iterate through the list of services.
+# Filter those whose status is **NOT** "UP" (i.e., those that are "DOWN" 
+# or "DEGRADED").
+# Save the problematic services into a new list using dictionaries or 
+# tuples (e.g., `{"name": "...", "status": "...", "latency": ...}`).
+
+## General Metrics
+#Calculate the availability percentage of your infrastructure:
+
+# Reporting
+#Export the result to a new JSON file named `alertas.json` containing
+#  only the incident report and the overall availability percentage.
+
 
 def guardar_file(services_with_problems):
 
@@ -61,3 +84,18 @@ def principal():
 if __name__ == "__main__":
 
     principal()
+
+
+#If we need get an API:
+
+#import requests
+
+# 1. Hacemos la petición a la API (pedimos datos)
+#response = requests.get("https://api.github.com/users/octocat")
+
+# 2. Convertimos la respuesta JSON directamente a un diccionario de Python
+#data = response.json()
+
+# 3. Usamos los datos como ya sabés hacer
+#print(f"Usuario: {data['login']}")
+#print(f"Repos públicos: {data['public_repos']}")
